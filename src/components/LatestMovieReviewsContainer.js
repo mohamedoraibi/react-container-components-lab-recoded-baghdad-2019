62a471955b43e3eeaa544cbdd5cd798baca7b2f4
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2020-01-15 20:19:36 +0000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,12 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+
+
+const LatestMovieReviewsContainer = ({ Movie }) => (
+  <div className="movie-list">
+    { movie.map(movie => <Movie title={movie.title} img_url={movie.img_url} />) }
+  </div>
+)
+ 
+export default LatestMovieReviewsContainer;
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,10 @@
-// Code MovieReviews Here
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+
+const MovieReviews = ({ Movie }) => (
+  <div className="movie-list">
+    { movie.map(movie => <Movie title={movie.title} img_url={movie.img_url} />) }
+  </div>
+)
+ 
+export default MovieReviews;
